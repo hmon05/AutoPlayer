73be9729d641589bcb12f8f69df259a1634d7898
hmontanobel <hmontanobel@gmail.com>
2025-04-17 21:45:41 +0000

    upgrede2

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ class App:
         self.notebook.add(self.tab_seleccion, text="Selección de Programa")
         self.notebook.add(self.tab_mapeo, text="Mapeo de Clics")
     
-        self.crear_tab_seleccion()  # Llama a crear_tab_seleccion aquí
+        
         self.crear_tab_mapeo()  # Llama a crear_tab_mapeo aquí
     
         self.selected_window = None
